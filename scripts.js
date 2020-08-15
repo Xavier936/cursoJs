b12e5a4fa51ef64aca862c3e0b78e3a0fdaec73a
Xavier936 <xavierlema1@gmail.com>
2020-08-15 15:21:30 -0500

    Sexta clase Condicional Switch

--- scripts.js
+++ scripts.js
@@ -1,42 +1,21 @@
-/* 
-Ordenar 3 numeros de mayor a menor
-Permutaciones con 3 elementos
-Posibilidades 3!= 3+2+1 = 6
-abc = 123
-acb = 132
-bac = 213
-bca = 231
-cab = 312
-cba = 321
-*/
+let num = 6;
 
-const numbers = document.getElementById('numbers');
-const result = document.getElementById('result');
+switch(num){
+    case 1: console.log('Num tiene el valor 1');
+    break;
+    case 2: console.log('Num tiene el valor 2');
+    break;
+    default: console.log('nNum no vale ni 1 ni 2');
+}
 
-let a= prompt('Introduca el primer numero');
-let b= prompt('Introduca el segundo numero');
-let c= prompt('Introduca el tercer numero');
-
-numbers.textContent = `Los numeros introducidos son ${a}, ${b}, ${c}`;
-
-/* Ejercicio */
-
-if(a >= b && a>=c){
-    if(b > c){
-        result.textContent = `El orden es: ${a}, ${b}, ${c} `;
-    }else{
-        result.textContent = `El orden es: ${a}, ${c}, ${b} `;
-    }
-} else if(b >= a && b>=c){
-    if(a > c){
-        result.textContent = `El orden es: ${b}, ${a}, ${c} `;
-    }else{
-        result.textContent = `El orden es: ${b}, ${c}, ${a} `;
-    }
-}else if(c >= a && c>=b){
-    if(a > b){
-        result.textContent = `El orden es: ${c}, ${a}, ${b} `;
-    }else{
-        result.textContent = `El orden es: ${c}, ${b}, ${a} `;
-    }
+switch(num){
+    case 1:
+    case 3:
+    case 5:
+    console.log(`${num} es impar`);
+    break;
+    case 2:
+    case 4:
+    case 6:
+    console.log(`${num} es par`);
 }
