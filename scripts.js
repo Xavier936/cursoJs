5db27e4a0e7124f91b13cd773509c4bd6da43970
Xavier936 <xavierlema1@gmail.com>
2020-08-14 20:46:10 -0500

    tercera clase manejo de cadenas

--- scripts.js
+++ scripts.js
@@ -1,22 +1,72 @@
-//operadores matematicos
+//manejo de cadenas o strings
 
-/*let a =5;
-let b = 2;
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);*/
+let cadena = "Hola Mundo";
 
-//operadores de incremento
-/*let a = 5;
+//Propiedades
 
-console.log(a);
-console.log(--a);*/
+//length-> devuelve la longitud de la cadena
+console .log(cadena.length);
 
-//operadores de asignacion
-let a = 10;
+//Metodos
 
-a+= 4;
+//los metodos devuelven una cadena nueva, la cadena original no se modifica
+//toUppercase()-> devuelve la cadena en mayusculas
 
-console.log(a);
+console.log(cadena.toUpperCase());
+/*let cadenaMayus = cadena.toUpperCase();
+console.log(cadenaMayus;
+*/
+
+//toLowerCase()-> devuelve la cadena en minusculas
+
+console.log(cadena.toLowerCase());
+
+//indexof(string)-> Devuelve la posicion en la que se encuentra el string, si no lo encuentra devuelve -1
+
+console.log(cadena.indexOf('Hola'));
+console.log(cadena.indexOf('o'));
+
+//repalce(valor a buscar, valor nuevo) -> reemplaza un fragmento de la cadena por un valor nuevo
+ console.log(cadena.replace('Mundo','Alexis'));
+
+ // substring(inicio [,fin])-> extrae los carateres de inicio a fin (el funa l no se incluye)
+ console.log(cadena.substring(3));
+ console.log(cadena.substring(3, 7));
+
+ //slice(inicio [,fin])-> igual que substring pero admite valores negativos, con esto empezara desde atras
+
+ console.log(cadena.slice(3));
+ console.log(cadena.slice(-3));
+ console.log(cadena.slice(0, -6));
+ //trim()-> elimina los espacions al inicio y final de la cadena
+ let cadena2= "     Hola a todos, estamos trabajando con cadenas   ";
+ console.log(cadena2);
+ console.log(cadena2.trim());
+
+ //startwhit()-> evalua si la cadna empieza con el valor que se indica, si coincide devlvera true
+console.log(cadena.startsWith('H'));
+ //endwhit()-> evalua si la cadena termina con el valor que se indica, si es asi devolvera true
+
+ console.log(cadena.endsWith('o'));
+
+ //includes(valor)-> misma funcion que indexof, con la diferencia que devuelve tru o false
+
+ console.log(cadena.includes('n'));
+ console.log(cadena.includes('a',5));
+
+ //repeat(string, valor)-> repite un numero de veces lo que se indica
+
+ let cadena3 = "hola";
+ console.log(cadena3.repeat(3));
+ console.log('r'.repeat(5));
+
+ //template strings
+
+ let nombre = "Alexis";
+ let apellido = "Lema";
+ let edad = 27;
+
+ console.log(`Hola ${nombre} ${apellido}. Tienes ${edad} años`);
+
+
+
