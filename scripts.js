8d08a85b2b2ac14f1391036da605b7ec0e69794a
Xavier936 <xavierlema1@gmail.com>
2020-08-16 14:37:56 -0500

    octava clase operador Arrays

--- scripts.js
+++ scripts.js
@@ -1,26 +1,24 @@
-//Operador ternario 
-//se usa cundo la condicion es true o false
+//Arrays
 /*
-simple: (condicion) ? true : false
-con mas condiciones:
- 
-(condicion) ?
-    (sentencia1, sentencia2)
-    :
-    (sentencia1, sentencia2)
+Son estructuras que nos permiten almacenar varios datos y agruparlos
+se puedn llenar con cualquier tipo de dato valido en Js y deben ir separados por comas
+Se pueden mezclar tipos de datos, pero no es recomendable
+Se declaran con llaves cuadaradas o corchetes []
+Pueden declararse vacios o con un contenido ya establecido
+Pueden añadirse o eliminarse en el momento que queramos
+let num = 5
+let array=[]
+let numeros=[1,2,3,4,5,6]
+Cada uno de los elementos podra ser identificado por su indice, es decir su posicion
+Los indices empiezan a contar desde 0
  */
 
-let num = 2;
+let numeros = [1,2,3,4,5];
+let palabras = ['Hola','estanos','en', 'aprendiendo'];
+let booleans = [true,true,false];
 
-(num % 2 == 0) ? console.log(`${num} es par`) : console.log(`${num} es impar`) 
+console.log(numeros[4] * numeros[3]);
+console.log(`La palabra "${palabras[3]}" tiene ${palabras[3].length} letras`);
 
-(num % 2 == 0) ?
-    (
-        console.log(`${num} es par`),
-        console.log(`${num} es par 2`)
-    )
-    :
-    (
-        console.log(`${num} es impar`),
-        console.log(`${num} es diferente de 2`)  
-    )
+
+
